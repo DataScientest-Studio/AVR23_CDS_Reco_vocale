3f7af0e68bd43ef2b4849c4ec3f54c96dcd3c746
Demosthene-OR <82312602+Demosthene-OR@users.noreply.github.com>
2023-06-09 12:19:32 +0200

    Update config.py with Team members

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -11,11 +11,32 @@ TITLE = "My Awesome App"
 
 TEAM_MEMBERS = [
     Member(
-        name="John Doe",
-        linkedin_url="https://www.linkedin.com/in/charlessuttonprofile/",
+        name="Keyne Dupont",
+        linkedin_url="https://www.linkedin.com/in/keyne-dupont/",
         github_url="https://github.com/charlessutton",
     ),
-    Member("Jane Doe"),
+    Member(
+        name="Yves Marc",
+        linkedin_url="https://www.linkedin.com/in/charlessuttonprofile/",
+        github_url="https://github.com/charlessutton",
+    ),    
+    Member(
+        name="Louise Pereira",
+        linkedin_url="https://www.linkedin.com/in/louise-edith-pereira-697168271/",
+        github_url="https://github.com/charlessutton",
+    ),
+    Member(
+        name="Tia Ratsimbason",
+        linkedin_url="https://www.linkedin.com/in/tia-ratsimbason-42110887/",
+        github_url="https://github.com/charlessutton",
+    ),    
+    Member(
+    name="Olivier Renouard",
+    linkedin_url="https://www.linkedin.com/in/olivier-renouard-b6b8a535/",
+    github_url="https://github.com/Demosthene-OR",
+    )
+    
+
 ]
 
-PROMOTION = "Promotion Bootcamp Data Scientist - April 2021"
+PROMOTION = "Promotion Continuous Data Scientist - April 2023"
