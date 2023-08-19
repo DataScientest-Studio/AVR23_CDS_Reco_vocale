607897aa17abd176e3c33dab4b7efd9171cadbc9
Demosthene-OR <olivier.renouard1103@gmail.com>
2023-08-19 22:19:02 +0200

    ...

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -36,7 +36,7 @@ TABS = OrderedDict(
 
 def run():
     st.sidebar.image(
-        "https://dst-studio-template.s3.eu-west-3.amazonaws.com/logo-datascientest.png",
+        "assets/logo-datascientest.png",
         width=200,
     )
     tab_name = st.sidebar.radio("", list(TABS.keys()), 0)
