d24462ac52393264ddfb84e8bc63b6f1b30f74c2
Demosthene-OR <olivier.renouard1103@gmail.com>
2023-08-30 11:32:21 +0200

    Update modelisation_dict_tab.py

--- streamlit_app/tabs/modelisation_dict_tab.py
+++ streamlit_app/tabs/modelisation_dict_tab.py
@@ -7,19 +7,27 @@ from PIL import Image
 title = "Traduction mot à mot"
 sidebar_name = "Traduction mot à mot"
 
-def display_dic(type, target_lang):
-    col1, col2, col3 = st.columns(3)
+def calcul_dic(Lang,Algo,Metrique):
+
+
+    if Algo=='Manuel':
+        df_dic = pd.read_csv('../data/dict_ref_'+Lang+'.csv',header=0,index_col=0, encoding ="utf-8", sep=';',keep_default_na=False).T
+    else:
+        df_dic = pd.read_csv('../data/dict_ref_'+Lang+'.csv',header=0,index_col=0, encoding ="utf-8", sep=';',keep_default_na=False).T
+    return df_dic
+
+def display_dic(df_dic, type, target_lang):
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
-        st.header(type)
-        st.image("https://static.streamlit.io/examples/cat.jpg")
-
+        st.dataframe(df_dic.T.iloc[:84], height=1500)
     with col2:
-        st.header(target_lang)
-        st.image("https://static.streamlit.io/examples/dog.jpg")
-
+        st.dataframe(df_dic.T.iloc[85:169], height=1500)
     with col3:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg")
+        st.dataframe(df_dic.T.iloc[170:254], height=1500)
+    with col4:
+        if len(df_dic.T)>254:
+             st.dataframe(df_dic.T.iloc[255:], height=1500)
+        else: st.write('')
 
 def run():
 
@@ -27,7 +35,8 @@ def run():
     st.write("## **Données d'entrée :**\n")
     Sens = st.radio('Sens :',('Anglais -> Français','Français -> Anglais'), horizontal=True)
     Lang = ('en_fr' if Sens=='Anglais -> Français' else 'fr_en')
-    Algo = st.radio('Algorithme :',('KMeans','KNN','Random Forest',' Word Embedding'), horizontal=True)
+    Algo = st.radio('Algorithme :',('Manuel', 'KMeans','KNN','Random Forest',' Word Embedding'), horizontal=True)
+    Metrique = ''
     if (Algo == 'KNN'):
         Metrique = st.radio('Metrique:',('minkowski', 'cosine', 'chebyshev', 'manhattan', 'euclidean'), horizontal=True)
     else:
@@ -35,5 +44,6 @@ def run():
         st.write("")
         st.write("")
         st.write("")
-    display_dic('ref',Lang)
+    df_dic = calcul_dic(Lang,Algo,Metrique)
+    display_dic(df_dic,'ref',Lang)
     
