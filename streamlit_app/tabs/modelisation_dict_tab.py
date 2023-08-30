84f8b4d2ea6f72620e8424a2164ee86c5819851a
Demosthene-OR <olivier.renouard1103@gmail.com>
2023-08-30 19:18:00 +0200

    Update modelisation_dict_tab.py

--- streamlit_app/tabs/modelisation_dict_tab.py
+++ streamlit_app/tabs/modelisation_dict_tab.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.cluster import KMeans
-from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.neighbors import KNeighborsClassifier
 
 
 title = "Traduction mot à mot"
