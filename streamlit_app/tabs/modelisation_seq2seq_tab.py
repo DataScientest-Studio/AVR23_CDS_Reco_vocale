35e8574e8718bf12cf019f72b675afa0ef8df8ae
Demosthene-OR <olivier.renouard1103@gmail.com>
2023-09-01 10:02:44 +0200

    ....

--- streamlit_app/tabs/modelisation_seq2seq_tab.py
+++ streamlit_app/tabs/modelisation_seq2seq_tab.py
@@ -1,16 +1,80 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
 from PIL import Image
-
+from transformers import pipeline
 
 title = "Traduction Sequence à Sequence"
 sidebar_name = "Traduction Seq2Seq"
 
+# !pip install transformers
+# !pip install sentencepiece
+
+@st.cache_data(ttl='1h00s')
+def load_corpus(path):
+    input_file = os.path.join(path)
+    with open(input_file, "r",  encoding="utf-8") as f:
+        data = f.read()
+        data = data.split('\n')
+        data=data[:-1]
+    return pd.DataFrame(data)
+
+n1 = 0
+df_data_en = load_corpus('../data/preprocess_txt_en')
+df_data_fr = load_corpus('../data/preprocess_txt_fr')
+translation_en_fr = pipeline('translation_en_to_fr', model="t5-base")
+translation_fr_en = pipeline('translation_fr_to_en', model="t5-base")
+
+
+def display_translation(n1, Lang):
+    global df_data_src, df_data_tgt
+
+    for i in range(n1,n1+5):
+        s = df_data_src.iloc[i][0]
+        source = Lang[:2]
+        target = Lang[-2:]
+        # for col in s.split():
+        #     st.write('col: '+col)
+        #     st.write('dict[col]! '+dict[col])
+        st.write("**"+source+"   :**  "+ s)
+        st.write("**"+target+"   :**  "+translation_model(s))
+        st.write("**ref. :** "+df_data_tgt.iloc[i][0])
+        st.write("")
+
 
 def run():
 
+    global n1, df_data_src, df_data_tgt, translation_model
+    global df_data_en, df_data_fr, lang_classifier, translation_en_fr, translation_fr_en
+
     st.title(title)
+
+    st.write("## **Données d'entrée :**\n")
+    Sens = st.radio('Sens :',('Anglais -> Français','Français -> Anglais'), horizontal=True)
+    Lang = ('en_fr' if Sens=='Anglais -> Français' else 'fr_en')
+
+    if (Lang=='en_fr'):
+        df_data_src = df_data_en
+        df_data_tgt = df_data_fr
+        translation_model = translation_en_fr
+    else:
+        df_data_src = df_data_fr
+        df_data_tgt = df_data_en
+        translation_model = translation_fr_en
+
+    
+    sentence1 = st.selectbox("Selectionnez la 1ere des 5 phrase à traduire avec le dictionnaire sélectionné", df_data_src.iloc[:-4],index=int(n1) )
+    n1 = df_data_src[df_data_src[0]==sentence1].index.values[0]
+
+
+    n1 = df_data_src[df_data_src[0]==sentence1].index.values[0]
+    lang_classifier = pipeline('text-classification',model="papluca/xlm-roberta-base-language-detection")
+
+    Lang_detected = lang_classifier (sentence1)
+    st.write(Lang_detected)
+    display_translation(n1, Lang)
+
 
     st.markdown(
         """
